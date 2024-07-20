e5f8fd26271a4db4366faef12e3d23bd2fbf11f0
renathossain <renathossain@gmail.com>
2024-07-19 22:51:43 -0400

    Merged branches "main" and "registration-attempy"

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,23 @@
-<script>
+<script lang="ts">
+  import { Router, Route } from "svelte-routing";
+  import Lobby from "./lib/Lobby.svelte";
   import Flappy from "./lib/Flappy.svelte";
+  import Player from "./lib/Player.svelte";
+
+  export let url = "";
 </script>
 
-<Flappy />
+<Router {url}>
+  <Route path="/"><Lobby /></Route>
+  <Route path="/game" component={Flappy} />
+  <Route path="/player/:username" let:params
+    ><Player username={params.username} /></Route
+  >
+</Router>
+
+<!-- <script>
+  import LandingPage from "./lib/LandingPage.svelte";
+</script>
+
+<link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
+<LandingPage /> -->
