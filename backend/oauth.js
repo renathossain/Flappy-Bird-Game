a5f48af23856bab9a2959e0a0a81e50d2ceea789
renathossain <renathossain@gmail.com>
2024-07-27 18:05:14 -0400

    fixed oauth

--- backend/oauth.js
+++ backend/oauth.js
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback",
+  callbackURL: process.env.CALLBACK_URL,
   passReqToCallback: true
 },
   async function (request, accessToken, refreshToken, profile, done) {
