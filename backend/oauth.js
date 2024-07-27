9e562eef3ff38e32dfd21252bb76d8696daa90fd
renathossain <renathossain@gmail.com>
2024-07-27 18:07:10 -0400

    update GOOGLE_CALLBACK_URL

--- backend/oauth.js
+++ backend/oauth.js
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.CALLBACK_URL,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
   passReqToCallback: true
 },
   async function (request, accessToken, refreshToken, profile, done) {
