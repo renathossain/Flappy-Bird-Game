98c8c5aca2786021e04021162c38546b6a0c5826
Anika2000 <anika.sultana@mail.utoronto.ca>
2024-07-27 16:22:09 -0400

    store

--- backend/models/skins.js
+++ backend/models/skins.js
@@ -7,12 +7,58 @@ export const Skin = sequelize.define("skins", {
     type: DataTypes.INTEGER,
     allowNull: false,
     primaryKey: true,
-  }
+  },
+  price : {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  imageMetadata: {
+    type: DataTypes.JSON,
+    allowNull: true,
+  },
 });
 
 // Ensure the skins table has predefined skins 1, 2, 3
 Skin.afterSync(async () => {
-  await Skin.findOrCreate({ where: { id: 1 } });
-  await Skin.findOrCreate({ where: { id: 2 } });
-  await Skin.findOrCreate({ where: { id: 3 } });
+
+  await Skin.findOrCreate({ where: { id: 1 },
+  defaults: {
+    price: 0,
+    imageMetadata: {
+      "filename": "1.png",
+      "originalname": "1.png",
+      "mimetype": "image/png",
+      "path": "/assets/flappies/1.png",
+    },
+  },
+  });
+
+    await Skin.findOrCreate({ where: { id: 2 },
+    defaults: {
+      price: 0,
+      imageMetadata: {
+        "filename": "2.png",
+        "originalname": "2.png",
+        "mimetype": "image/png",
+        "path": "/assets/flappies/2.png",
+      },
+    },
+    });
+
+
+    await Skin.findOrCreate({ where: { id: 3 },
+      defaults: {
+        price: 0,
+        imageMetadata: {
+          "filename": "3.png",
+          "originalname": "3.png",
+          "mimetype": "image/png",
+          "path": "/assets/flappies/3.png",
+        },
+      }
+    });
 });
+
+
+
+      
