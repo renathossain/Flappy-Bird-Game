449f5fa8014c50b4dcbb447ccc3043b259eadd24
Anika2000 <anika.sultana@mail.utoronto.ca>
2024-07-27 16:44:40 -0400

    adding leftover

---
+++ frontend/src/lib/Store.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+    //https://medium.com/@jack.dixon.ryan/sveltequest-iii-making-api-calls-fffda33e5527
+    import Card from "./components/Card.svelte";
+    import type { Skin } from "../types/skin";
+    import Button from "./components/Button.svelte";
+  import { onMount } from "svelte";
+
+    let skins: Skin[] = [];
+    const backendBaseUrl = "http://localhost:3000";
+    let prevCursor:number|null = null;
+    let nextCursor:number|null = null;
+    let limit = 5;
+
+    async function getSkins(cursor: number|null, action:string|null) {
+        //const res = await fetch(`${backendBaseUrl}/api/skin/all`);
+        const res = await fetch(`${backendBaseUrl}/api/skin/?cursor=${cursor}&action=${action}`);
+        const data = await res.json();
+        skins = data.data.map((skin: any) => {
+            return {
+            id: skin.id, 
+            price: skin.price,
+            imagePath: skin.imageMetadata.path,
+            }
+        });
+        prevCursor = data.prev;
+        nextCursor = data.next;
+    }
+
+    function handleNext(){
+       if (nextCursor) getSkins(nextCursor, 'next');
+    }
+    function handlePrev(){
+        console.log('prevCursor:', prevCursor);
+        if (prevCursor) getSkins(prevCursor, 'prev');
+    }
+
+    onMount(() => {
+        getSkins(null, null);
+    });
+
+    
+
+</script>
+
+<main>
+    <div class='header'>
+        <!-- maybe needs registration because users can do /store and get to this route without being registered -->
+        <Button text="Sign Out" link="/auth/logout"></Button>
+    </div>
+    <div class='container'>
+        <div class='skins'>
+            {#each skins as skin}
+                <Card id={skin.id} price={skin.price} 
+                image={skin.id <= 3 ? skin.imagePath: `${backendBaseUrl}/${skin.imagePath}`}
+                buttonText={skin.id <= 3? "User Skin": "Purchase"}></Card>
+            {/each}
+        </div>
+        <div class="buttons-pagination">
+            {#if prevCursor !== null && prevCursor !== 1}
+                <button class="button-pag" on:click={handlePrev}>Previous</button>
+            {/if}
+            {#if nextCursor != null && nextCursor !== 20}
+                <button class="button-pag next-button" on:click={handleNext}>Next</button>
+            {/if}
+        </div>
+    </div>
+</main>
+
+<style>
+    .header {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+    }
+    .container{
+        width: 90vw;
+        height: 80vh;
+        margin: 20px;
+        display: flex;
+        flex-direction: column;
+        padding: 20px;
+    }
+    .skins{
+        margin-bottom: 20px;
+        flex: auto;
+        display: flex;
+        column-gap: 20px;
+    }
+    .buttons-pagination{
+        display: flex;
+        justify-content: space-between;
+        position: absolute;
+        gap: 20px;
+        bottom: 60px;
+        padding: 0 20px;
+        width: 85vw;
+    }
+    .button-pag{
+        background-color: yellow;
+        font-family: "RetroGaming", sans-serif;
+        font-size: 20px;
+        cursor: pointer;
+    }
+    .next-button{
+        margin-left: auto;
+    }
+</style> 
---
+++ frontend/src/lib/components/Card.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+    //https://codepen.io/paulnoble/pen/gaPaoY
+    //https://codepen.io/Brandon-Stoyles/pen/RajYmd
+    //https://docs.stripe.com/js/deprecated/redirect_to_checkout
+    //https://stackoverflow.com/questions/68479217/how-to-load-environment-variables-in-svelte-using-vite-or-svite
+
+  import { loadStripe } from '@stripe/stripe-js';
+  import { onMount } from 'svelte';
+
+    export let id: number;
+    export let price: number;
+    export let image: string;
+    export let buttonText: string;
+    let stripe;
+
+    onMount(async () => {
+      stripe = await loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+    });
+
+    const backendBaseUrl = "http://localhost:3000";
+
+    async function handlePurchase(price: number, currency: string) {
+      const response = await fetch(`${backendBaseUrl}/api/stripe/charge`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          price: price,
+          currency: currency,
+        })
+      });
+      const data = await response.json();
+      if (data.url){
+        window.location.href = data.url;
+      }else{
+        console.log('Issue creating Stripe payment process', data.error);
+      }
+    }
+
+    async function handleUseSkin(){
+
+    }
+  </script>
+  
+  <div class='arcade-container'>
+    <div class='arcade-text'>
+      Skin No: {id}
+    </div>
+    <div class='arcade-image'>
+      <img src={image} alt={'src/assets/picture.png'}/>
+    </div>
+    <div class='arcade-price'>
+      <p>Price: ${price}</p>
+      {#if buttonText === 'Purchase'}
+        <button class='arcade-button' on:click={() => handlePurchase(price, 'usd')}>{buttonText}</button>
+      {:else}
+        <button class='arcade-button'>{buttonText}</button>
+      {/if}
+    </div>
+  </div>
+  
+  <style>
+    .arcade-text {
+
+  color: white;
+        position: absolute;
+        width: 100%;
+        text-align: center;
+        margin: auto;
+      font-size: 2vw;
+      margin-bottom: 10px;
+      text-shadow: -2px 0 0 #fdff2a,
+    -4px 0 0 #df4a42,
+    2px 0 0 #91fcfe,
+    4px 0 0 #4405fc;
+    }
+    .arcade-container {
+      backdrop-filter: blur(10px);
+      background-color: rgba(255, 255, 255, 0.2);
+      display: flex;
+      flex-direction: column;
+      width: 90%;
+      max-width: 600px;
+      border: 10px solid orange;
+      margin: 50px auto;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+      color: red;
+      font-family: 'Press Start 2P', cursive;
+      text-align: center;
+    }
+    .arcade-image{
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .arcade-image img{
+        width: 100px;
+        max-height: 100px;
+        object-fit: cover;
+    }
+
+    .arcade-button{
+	background: #881400;
+	border-bottom: 6px inset rgba(0,0,0,.5);
+	border-left: 6px inset rgba(0,0,0,.5);
+	border-right: 6px inset rgba(255,255,255,.5);
+	border-top: 6px inset rgba(255,255,255,.5);
+	color: white;
+	font-size: 1rem;
+	margin: .5rem;
+	min-width: 100px;
+	padding: .5rem;
+    cursor: pointer;
+	&:hover {
+		background: #A81000;
+	}
+}
+  </style>
